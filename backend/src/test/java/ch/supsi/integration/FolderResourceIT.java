28330faf961ece30fab89b6ca981974ed07aebfa
luca.fanto <luca.fanto@supsi.ch>
2025-03-01 00:54:59 +0100

    spike: fix, removing @QuarkusIntegrationTest and using @QuarkusTest

--- backend/src/test/java/ch/supsi/integration/FolderResourceIT.java
+++ backend/src/test/java/ch/supsi/integration/FolderResourceIT.java
@@ -3,7 +3,6 @@ package ch.supsi.integration;
 import ch.supsi.model.api.Folder;
 import ch.supsi.service.testContainersResource.MongoTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import jakarta.ws.rs.core.Response;
@@ -12,7 +11,7 @@ import org.junit.jupiter.api.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-@QuarkusIntegrationTest
+@QuarkusTest
 @QuarkusTestResource(MongoTestResource.class)
 @Tag("integration")
 public class FolderResourceIT {
