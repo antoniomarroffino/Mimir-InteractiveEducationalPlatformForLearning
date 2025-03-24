70da075b33fede74028940a0c46798db08e9bf78
antoniomarroffino <65792753+antoniomarroffino@users.noreply.github.com>
2025-03-24 09:52:59 +0100

    feature: - added delete and update options in backend for courses

--- backend/src/main/java/ch/supsi/repository/CourseRepository.java
+++ backend/src/main/java/ch/supsi/repository/CourseRepository.java
@@ -9,12 +9,9 @@ import java.util.List;
 
 @ApplicationScoped
 public class CourseRepository implements PanacheMongoRepository<Course> {
-    // Implementa esplicitamente il metodo listAll
     public List<Course> listAll() {
         return mongoCollection().find().into(new ArrayList<>());
     }
-
-    // Oppure, se preferisci usare la query di Panache
     public List<Course> findAllCourses() {
         return list("{}");
     }
