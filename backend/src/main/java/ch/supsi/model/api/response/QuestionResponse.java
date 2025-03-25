7512ff25e5c462f010bc3868f0c5f6a66708bc12
antoniomarroffino <65792753+antoniomarroffino@users.noreply.github.com>
2025-03-25 09:55:37 +0100

    feature: - created base Response model and QuizAttempt

---
+++ backend/src/main/java/ch/supsi/model/api/response/QuestionResponse.java
@@ -0,0 +1,29 @@
+package ch.supsi.model.api.response;
+
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.bson.codecs.pojo.annotations.BsonDiscriminator;
+import org.bson.types.ObjectId;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(description = "Base Question Response model", name = "QuestionResponse")
+@BsonDiscriminator(key = "type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = TrueFalseResponse.class, name = "TRUE_FALSE"),
+        @JsonSubTypes.Type(value = MultipleChoiceResponse.class, name = "MULTIPLE_CHOICE")
+})
+public abstract class QuestionResponse {
+    public ObjectId id;
+    public ResponseType type;
+
+    protected QuestionResponse() {
+        this.id = new ObjectId();
+    }
+
+    protected QuestionResponse(ResponseType type) {
+        this();
+        this.type = type;
+    }
+}
---
+++ backend/src/main/java/ch/supsi/model/api/response/ResponseType.java
@@ -0,0 +1,6 @@
+package ch.supsi.model.api.response;
+
+public enum ResponseType {
+    TRUE_FALSE,
+    MULTIPLE_CHOICE
+}
