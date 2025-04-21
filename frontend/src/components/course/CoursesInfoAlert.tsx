3788cee7d9b617fdd9a6a29e6ec096aec1709a1d
antoniomarroffino <65792753+antoniomarroffino@users.noreply.github.com>
2025-04-21 10:21:27 +0200

    feature: - created new component to improve layout for badges

--- frontend/src/components/course/CoursesInfoAlert.tsx
+++ frontend/src/components/course/CoursesInfoAlert.tsx
@@ -1,9 +1,9 @@
 import {PageInfoAlert} from "../common/PageInfoAlert";
-import {AcademicCapIcon} from "@heroicons/react/24/solid";
+import {FiBook} from "react-icons/fi";
 
 export const CoursesInfoAlert = () => (
     <PageInfoAlert
-        icon={<AcademicCapIcon className="w-5 h-5" />}
+        icon={<FiBook className="w-5 h-5" />}
         title="Manage Your Courses"
         message="Create new learning spaces for your students, organize educational content, and track your teaching impact. Each course you create is a new opportunity to spark engagement and foster growth."
         colorFrom="from-indigo-50"
--- frontend/src/components/course/CoursesPageHeader.tsx
+++ frontend/src/components/course/CoursesPageHeader.tsx
@@ -1,6 +1,6 @@
-import {AcademicCapIcon} from '@heroicons/react/24/outline';
 import {BasePageHeader} from '../common/BasePageHeader';
 import React from 'react';
+import {FiBook} from "react-icons/fi";
 
 interface CoursesPageHeaderProps {
     onToggleInfo: () => void;
@@ -14,7 +14,7 @@ export const CoursesPageHeader: React.FC<CoursesPageHeaderProps> = ({
     <BasePageHeader
         title="Your Learning Playground"
         subtitle="Craft engaging learning experiences that inspire and transform"
-        icon={<AcademicCapIcon className="w-10 h-10 text-white" />}
+        icon={<FiBook className="w-10 h-10 text-white" />}
         gradientFrom="from-blue-600"
         gradientTo="to-cyan-600"
         showInfoToggle={showInfoToggle}
